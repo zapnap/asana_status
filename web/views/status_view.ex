8a790b842d5ff61a0b324c359965e2b4f51780f8
Nick Plante <nap@zerosum.org>
2015-01-20 18:07:38 -0500

    status labeling updates

--- web/views/status_view.ex
+++ web/views/status_view.ex
@@ -1,28 +1,28 @@
 defmodule AsanaStatus.StatusView do
-  require Logger
   use AsanaStatus.View
 
+  @status_map %{
+    "active" => :p1,
+    "hold" => :p2,
+    "queued" => :p2,
+    "done" => :p3,
+    "cold" => :p3 }
+
   def project_status(project) do
-    status_map = %{
-      "queued" => "queued",
-      "on hold" => "queued",
-      "done" => "done",
-      "to print" => "done",
-      "waiting" => "waiting",
-      "proofed" => "waiting",
-      "comps sent" => "waiting"
-    }
     notes = String.downcase(project.notes)
-    status_match = Enum.find(Map.keys(status_map), fn(k) -> Regex.match?(~r/#{k}/, notes) end)
+    status_match = Enum.find(Map.keys(@status_map), fn(k) -> Regex.match?(~r/#{k}/, notes) end)
     if status_match do
-      status_map[status_match]
+      @status_map[status_match]
     else
-      nil
+      :px
     end
   end
 
+  def sorted_by_status(projects) do
+    Enum.map(projects, fn(project) -> { project_status(project), project.name, project } end) |> Enum.sort
+  end
+
   def project_members(project, users) do
-    Logger.debug "project members"
     ids = Enum.map(project.members, fn(m) -> m["id"] end)
     Enum.filter(users, fn(u) -> Enum.member?(ids, u.id) end)
   end
@@ -34,7 +34,6 @@ defmodule AsanaStatus.StatusView do
   end
 
   def user_icon(user, size \\ "60x60") do
-    Logger.debug "user icon"
     name = user.name
     src = nil
 
